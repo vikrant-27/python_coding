1eb11078ac895f4865bf34f94dac3f9d34512979
vikrant sarade <101873319+vikrant-27@users.noreply.github.com>
2024-08-08 22:28:41 +0530

    Add files via upload

---
+++ squareRoot.py
@@ -0,0 +1,10 @@
+import math
+#using exponentiation to find (square root)
+num=64
+sr=num**(1/2) #or 0.5
+print("square root is" ,sr)
+
+#using math module
+num1=36
+sr1 = math.sqrt(num1)
+print(sr1)
